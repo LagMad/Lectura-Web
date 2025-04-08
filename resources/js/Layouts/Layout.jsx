6c74eef6ef77c0e3babccf47bb42e996736315d8
faracutrisfa <faracutrisfa@gmail.com>
2025-04-08 09:30:00 +0700

    feat: add navbar and footer to layout

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Layout = ({ children }) => {
     return (
         <>
             <header>
-                <NavBar />
+                <Navbar />
             </header>
 
-            <main className=" h-max md:min-h-screen z-0 bg-cust-white overflow-hidden">
+            <main className=" h-max mpred:min-h-screen z-0 bg-cust-white overflow-hidden">
                 {children}
             </main>
             <Footer />
