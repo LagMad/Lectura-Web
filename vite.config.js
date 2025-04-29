f7a52136dae90618469767262cbea8ee256ad13f
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2025-04-29 16:43:25 +0700

    fixing tailwindcss 4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx", // File utama JS
             refresh: true,
         }),
         react(),
