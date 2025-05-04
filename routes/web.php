b2762af076ae2cc52555391c9f7302ee079f7037
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2025-05-04 20:57:22 +0700

    feat: Add role to User (Admin,Siswa)

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+    return Inertia::render('Home');
+})->name('home');
 
 Route::get('/buku', [BookController::class, 'index'])->name('books.index');
 Route::get('/admin-tambah-buku', [BookController::class, 'create'])->name('books.create');
@@ -40,10 +35,6 @@ Route::get('/admin-pengaturan', function () {
     return Inertia::render('Admin/Pengaturan');
 });
 
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
