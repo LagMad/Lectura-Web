a8be5158a1afa652e98f75c924d20e45741a9b49
Jeremmy <hizkiajeremmy@gmail.com>
2025-05-05 22:20:02 +0700

    fix: Button cursor-pointer

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Button = ({ children, variant = 'outline', ...props }) => {
-    const base = 'font-semibold text-sm rounded-lg px-3 py-2 lg:px-5 lg:py-2 hover:scale-105 transition-all';
-    const outline = 'border-2 border-cust-primary-color text-cust-primary-color';
-    const filled = 'bg-cust-primary-color border-2 border-cust-primary-color text-white';
+    const base = 'font-semibold text-sm rounded-lg px-3 py-2 lg:px-5 lg:py-2 hover:scale-105 transition-all cursor-pointer';
+    const outline = 'border-2 border-cust-primary-color text-cust-primary-color cursor-pointer';
+    const filled = 'bg-cust-primary-color border-2 border-cust-primary-color text-white cursor-pointer';
 
     const classes = `${base} ${variant === 'filled' ? filled : outline}`;
 
