7ae11d8b436c60027e30bd4412399a2ad1642bc8
Jeremmy <hizkiajeremmy@gmail.com>
2025-05-12 12:15:48 +0700

    feat: hide/show password in register and login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -85,9 +85,8 @@ export default function Login({ status, canResetPassword }) {
                                 >
                                     Kata Sandi
                                 </label>
-                                <input
+                                <TextInput
                                     id="password"
-                                    type="password"
                                     name="password"
                                     value={data.password}
                                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
