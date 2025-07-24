6e94b1bae833e0372ed9accad8b68e35986c568d
Jeremmy <hizkiajeremmy@gmail.com>
2025-07-24 23:39:15 +0700

    adjustment to hero section in tentang page

--- resources/js/Sections/Tentang/Hero.jsx
+++ resources/js/Sections/Tentang/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HeroImage from "../../../../public/hero-profile.webp";
 import Button from "@/Components/Button";
 
 const Hero = ({ staff, books, user }) => {
@@ -11,28 +10,29 @@ const Hero = ({ staff, books, user }) => {
     ];
 
     return (
-        <section className="">
+        <section>
             <div className="relative flex justify-start items-center bg-[url('/fotbar.jpg')] bg-center bg-cover bg-no-repeat h-screen px-5 sm:px-10 md:px-16 lg:px-20 xl:px-40 py-6">
                 {/* Overlay */}
                 <div className="absolute inset-0 bg-white/80 z-0" />
 
                 {/* Content */}
-                <div className="relative z-10 flex flex-col lg:flex-row items-center justify-start gap-10 w-full lg:w-1/2">
-                    <div className="w-full space-y-4 text-center lg:text-left">
-                        <h1 className="font-bold text-3xl lg:text-4xl">
-                            Bertemu dengan Tim Perpustakaan
+                <div className="relative z-10 flex flex-col lg:flex-row items-center justify-start gap-0 md:gap-10 w-full">
+                    <div className="w-full md:w-1/2 space-y-4 text-center lg:text-left">
+                        <h1 className="font-bold text-3xl lg:text-4xl text-cust-dark">
+                            Selamat Datang di E-Library PUMA Rymba
                         </h1>
                         <p className="text-sm sm:text-base lg:text-lg text-gray-700">
-                            Profesional yang berdedikasi berkomitmen untuk
-                            mendukung perjalanan akademik dan kebutuhan
-                            penelitian Anda.
+                            Perpustakaan digital milik <strong>SMAN 1 Batu</strong> yang
+                            dikembangkan oleh <strong>Lectura</strong> dari <strong>Tanoto Foundation</strong>,
+                            menghadirkan akses pengetahuan tanpa batas bagi seluruh siswa dan tenaga pendidik.
                         </p>
-                        <div className="flex flex-row justify-center lg:justify-start gap-3 mt-6">
-                            <Button variant="filled">Mulai Perjalanan</Button>
-                            <Button variant="filledReverse">
-                                Jam Perpustakaan
-                            </Button>
-                        </div>
+                        {/* <div className="flex flex-row justify-center lg:justify-start gap-3 mt-6">
+                            <Button variant="filled">Jelajahi Koleksi</Button>
+                            <Button variant="filledReverse">Tentang Lectura</Button>
+                        </div> */}
+                    </div>
+                    <div className="w-full md:w-1/2 justify-center items-center">
+                        <img className="w-full md:w-4/5 lg:w-2/3 self-center mx-auto" src="/Logo-lectura-full-transparent.svg"/>
                     </div>
                 </div>
             </div>
--- resources/js/Sections/Tentang/WebPortal.jsx
+++ resources/js/Sections/Tentang/WebPortal.jsx
@@ -12,7 +12,7 @@ const WebPortal = ({ web }) => {
                     </p>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 md:gap-10 justify-center items-center">
-                    {web ? (
+                    {web.length > 0 ? (
                         web.map((web) => (
                             <button
                                 onClick={() =>
